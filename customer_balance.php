c62ae330732d5b59d8e0c9566da875084f2e2727
sgloverisme <89475656+sgloverisme@users.noreply.github.com>
2022-10-16 17:29:39 -0700

    Create customer_balance.php

---
+++ customer_balance.php
@@ -0,0 +1,20 @@
+<?php
+
+    $sql = "SELECT firstName, lastName, balance FROM customer 
+    WHERE balance !=0;";
+    $result = mysqli_query($link,$sql);
+    $row = mysqli_fetch_row($result);
+    print "<pre>";
+    print "<table border=1>";
+    print "<tr><td>Account id </td><td> Video id </td>";
+    while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
+    {
+        print "\n";
+        print "<tr><td>$row[accountid] </td><td> $row[videoid] </td><tr> ";
+    }
+    print "</table>";
+    print "</pre>";
+    mysqli_free_result($result);
+    mysqli_close($link);
+?>
+</center>
